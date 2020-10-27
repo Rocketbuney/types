f650abda0d75666edee972bb58be19843b9e8e43
Jack Thake <jackthake@hotmail.com>
2020-10-27 16:12:41 -0700

    :white_check_mark: Added types::stack tests

--- test/stack_test.cpp
+++ test/stack_test.cpp
@@ -11,4 +11,50 @@ namespace {
     types::stack stack;
     EXPECT_EQ(stack.isempty(), true);
   }
+
+  TEST(stack, push_to_empty_stack) {
+    types::stack stack(10);
+    stack.push(1);
+    EXPECT_EQ(stack.isempty(), false);
+  }
+
+  TEST(stack, push_to_stack_with_no_length) {
+    types::stack stack;
+    stack.push(1);
+    EXPECT_EQ(stack.isempty(), true);
+  }
+
+  TEST(stack, peak_from_empty_stack) {
+    types::stack stack;
+    int temp = 15; /* arbitrary value */
+
+    EXPECT_EQ(stack.isempty(), true);
+    EXPECT_EQ(stack.peak(temp), false);
+    EXPECT_EQ(15, temp); /* make sure temp wasn't modified */
+  }
+
+  TEST(stack, peak_from_populated_stack) {
+    types::stack stack(1);
+    int temp = 0;
+
+    stack.push(1);
+    EXPECT_EQ(stack.isempty(), false);
+    EXPECT_EQ(stack.peak(temp), true);
+    EXPECT_EQ(1, temp);
+  }
+
+  TEST(stack, pop_from_empty_stack) {
+    types::stack stack;
+
+    EXPECT_EQ(stack.isempty(), true);
+    EXPECT_FALSE(stack.pop());
+  }
+
+  TEST(stack, popk_from_populated_stack) {
+    types::stack stack(1);
+
+    stack.push(1);
+    EXPECT_EQ(stack.isempty(), false);
+    EXPECT_TRUE(stack.pop());
+  }
 };
